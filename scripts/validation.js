937e5c162083d5f7372c1bf3490b0d55a6e3a34f
Trang T <trangmtruong@yahoo.com>
2023-09-18 13:14:55 -0700

    add config and enableValidation func

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,6 +1,14 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((inputEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+  });
+}
 const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
@@ -10,20 +18,32 @@ const config = {
   errorClass: "modal__error_visible",
 };
 
+enableValidation(config);
+
+//red error message displayed if not passing validation ??
+//use default error messages from browser ??
+//if ANY field doesnt pass validation >> inactive
+//see Figma for colors of inactive button ??
+//button should be inactive when open modal with no values
+
+/*
 const formElement = document.querySelector(config.formSelector);
 const formInput = formElement.querySelector(config.inputSelector);
+errorMessage = formElement.querySelectorAll(config.inputErrorClass);
+*/
 
+/*
 const showInputError = (formElement, formInput, errorMessage) => {
-  const formError = formElement.querySelector(`${formInput.id}-error`);
-  formInput.classList.add("modal__input_type_error");
+  const formError = formElement.querySelector(`#${formInput.id}-error`);
+  formInput.classList.add(config.inputErrorClass);
   formError.textContent = errorMessage;
-  formError.classList.add("modal__error_visible");
+  formError.classList.add(config.errorClass);
 };
 
-const hideInputError = (element) => {
-  const formError = formElement.querySelector(`${formInput.id}-error`);
-  formInput.classList.remove("modal__input_type_error");
-  formError.classList.remove("modal__error_visible");
+const hideInputError = (formElement, formInput) => {
+  const formError = formElement.querySelector(`#${formInput.id}-error`);
+  formInput.classList.remove(config.inputErrorClass);
+  formError.classList.remove(config.errorClass);
   formError.textContent = "";
 };
 
@@ -34,8 +54,36 @@ const checkInputValidity = (formElement, formInput) => {
     hideInputError(formElement, formInput);
   }
 };
+/*
+const hasInvalidInput = ();
+const toggleButtonState = ();
+*/
+//Event Handlers
+/*
+const setEventListeners = (formElement, options) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(options.inputSelector)
+  );
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
 
-formElement.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-});
-formInput.addEventListener("input", checkInputValidity);
+const enableValidation = (options) => {
+  const formList = Array.from(document.querySelectorAll(options.formSelector));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement, options); //??
+  });
+};
+
+enableValidation(config);
+
+//closing popup by pressing on overlay
+//closing popup by pressing esc
+*/
